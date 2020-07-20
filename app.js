2032b21ce1b056ff98853f8131ffdb5dd2d44de3
Valmyr Tavares <valmyr.lima@millennium.com.br>
2020-07-19 21:40:40 -0300

    E vamos entrar dentro do Mongo, do banco de dados Mongo, simples assim, dentro do models comtamos um modele que se parece muito com um objeto e vamos mandar os dados a partir desse model. Inacreditavelmente simples

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require ('body-parser');
 const app = express();
 const admin = require("./routes/admin")
 const path = require("path")
-//const mongoose = require("mongoose")
+const mongoose = require("mongoose")
 
 //Configurações
 // BODY PARSER
@@ -14,6 +14,13 @@ app.use(bodyParser.json())
 //HANDLEBARS
 app.engine('handlebars', handlebars({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars');
+//Mongose
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost/blogapp").then(()=>{
+    console.log("Conectado ao mongo")
+}).catch((err)=>{
+    console.log("Erro ao se conectar" + err) 
+})
 // Public
     app.use(express.static(path.join(__dirname, "public")))
 //Rotas
---
+++ models/Categoria.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema;
+
+const Categoria = new Schema({
+    nome:{
+        type:String,
+        required:true
+    },
+    slug:{
+        type:String,
+        required:true
+    },
+    nome:{
+        type:Date,
+       default: Date.now()
+    }
+})
+mongoose.model("categorias", Categoria)
