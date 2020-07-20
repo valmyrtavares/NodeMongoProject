4ab5c3f83ed19944c333f7d80e868b785bc09d2e
Valmyr Tavares <valmyr.lima@millennium.com.br>
2020-07-20 15:57:46 -0300

    Essa aula que passou é a 39. Ele instalou um novo espress e um flash. E fez um middware dentro do app.js. Sem dificuldades até aqui

--- app.js
+++ app.js
@@ -6,8 +6,23 @@ const app = express();
 const admin = require("./routes/admin")
 const path = require("path")
 const mongoose = require("mongoose")
+const session = require('express-session')
+const flash = require("connect-flash")
 
 //Configurações
+//Sessção
+app.use(session({
+    secret:"CursoDeNode",
+    resave: true,
+    saveUninitialized: true
+}))
+app.use(flash())
+//Middleware
+app.use((req, res, next)=>{
+    res.locals.success_msg = req.flash("success_msg")
+    res.locals.error_msg = req.flesh("error_msg")
+    next()
+})
 // BODY PARSER
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
@@ -23,6 +38,11 @@ mongoose.connect("mongodb://localhost/blogapp").then(()=>{
 })
 // Public
     app.use(express.static(path.join(__dirname, "public")))
+
+    app.use((req, res, next)=>{
+        console.log("OI EU SOU UM MIDWARE")
+        next();
+    })
 //Rotas
 app.get('/', (req, res) =>{
     res.send("Rota Principal")
