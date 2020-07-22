55c05981a293a2b03b23964732b767e0c2355ae8
Valmyr Tavares <valmyr.lima@millennium.com.br>
2020-07-22 15:10:26 -0300

    Esse é o commit da aula 41, Ele mostra a lista de itens cadastrados em uma view. No arquivo admin.js é necessário acrescentar .lean() logo depois do find(). Fica assim Categoria.find().lean()  e isso faz com que a lista funcione. E isso está no forum da aula.

--- routes/admin.js
+++ routes/admin.js
@@ -13,7 +13,12 @@ router.get('/posts', (req, res)=> {
 })
 
 router.get('/categorias', (req, res)=> {
-    res.render("admin/categorias")
+  Categoria.find().lean().then((categorias)=>{
+      res.render("admin/categorias",{categorias: categorias})
+  }).catch((err)=> {
+      req.flash("erro_msg", "Houve um erro ao listar as categorias")
+      res.redirect("/admin")
+  })
 })
 
 router.get("/categorias/add", (req, res)=> {
