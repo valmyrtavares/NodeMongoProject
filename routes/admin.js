229035e339290b7c013509c58125ff72c12601ad
Valmyr Tavares <valmyr.lima@millennium.com.br>
2020-08-04 12:22:00 -0300

    Adicionado no formulario de postagem uma nova postagem com validações. Nesse codigo eu testei e deu certo, eu ainda não testei as validações de erro AULA 46

--- routes/admin.js
+++ routes/admin.js
@@ -3,6 +3,8 @@ const router = express.Router()
 const mongoose = require("mongoose")
 require("../models/Categoria")
 const Categoria = mongoose.model("categorias")
+require('../models/Postagem')
+const Postagem = mongoose.model("postagens")
 
 router.get('/', (req, res)=> {
    res.render("admin/index")
@@ -108,6 +110,34 @@ router.get("/postagens/add",(req, res)=> {
         res.redirect("/admin")
     })
 } )
+router.post("/postagens/nova", (req, res) => {
+
+    var erros = []
+
+    if(req.body.categoria == "0"){
+        erros.push({text: "Categoria incalida, registre uma categoria"})
+    }
+
+    if(erros.length > 0){
+        res.render("admin/addpostagem", {erros: erros})
+    }else{
+        const novaPostagem = {
+            titulo: req.body.titulo,
+            descricao: req.body.descricao,
+            conteudo: req.body.conteudo,
+            categoria: req.body.categoria,
+            slug: req.body.slug,
+
+        }
+        new Postagem(novaPostagem).save().then(()=>{
+            req.flash("success_msg", "Postagem criada com sucesso")
+            res.redirect("/admin/postagens")
+        }).catch((err)=> {
+            req.flash("error_msg", "Houve um erro durante o salvamento da postagem")
+            res.redirect("/admin/postagens")
+        })
+    }
+})
 
 
 
