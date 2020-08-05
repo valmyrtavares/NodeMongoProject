65078d313a611771bb88f8be0293cef5dc2bd2dd
Valmyr Tavares <valmyr.lima@millennium.com.br>
2020-08-05 15:26:02 -0300

    Feito a edição e mostra das postagens AUAL 48

--- routes/admin.js
+++ routes/admin.js
@@ -143,6 +143,50 @@ router.post("/postagens/nova", (req, res) => {
     }
 })
 
+router.get("/postagens/edit/:id", (req, res) =>{
+
+    Postagem.findOne({_id: req.params.id}).lean().then((postagem)=>{
+
+        Categoria.find().then((categorias)=> {
+        res.render("admin/editpostagens", {categorias: categorias, postagem: postagem})
+        }).catch((err)=>{
+            req.flash("error_msg", "Houve um erro ao listar as categorias")
+            res.redirect("/admin/postagens")
+        })
+
+    }).catch((err)=>{
+        req.flash("erro_msg", "Houve um erro ao carregar o formulario de edição")
+        res.redirect("admin/postagens")
+    })    
+   
+})
+
+router.post("/postagens/edit", (req, res) =>{
+
+    Postagem.findOne({_id: req.body.id}).then((postagem)=>{
+
+        postagem.titulo = req.body.titulo
+        postagem.slug = req.body.slug
+        postagem.descricao = req.body.descricao
+        postagem.conteudo - req.body.conteudo
+        postagem.categoria - req.body.categoria
+
+        postagem.save().then(()=> {
+            req.flash("success_msg", "Postagem editada com sucesso")
+            res.redirect("/admin/postagens")
+        }).catch((err) => {
+            req.flash("erro_msg", "Erro interno")
+            res.redirect("/admin/postagens")
+        })
+
+        
+    }).catch((err)=> {
+        req.flash("error_msg", "Houve um erro ao salvar edição")
+        res.redirect("/admin/postagens")
+    })
+
+})
+
 
 
 
