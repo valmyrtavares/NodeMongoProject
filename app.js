205a6c1a483d9708b178e17a83e66e4aa7b4da02
Valmyr Tavares <valmyr.lima@millennium.com.br>
2020-08-05 18:07:03 -0300

    Aula 50 listando as postagens na pagina index. E aqui tem o lean() again

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const path = require("path")
 const mongoose = require("mongoose")
 const session = require('express-session')
 const flash = require("connect-flash")
+require("./models/Postagem")
+const Postagem = mongoose.model("postagens")
 
 //Configurações
 //Sessção
@@ -45,8 +47,20 @@ mongoose.connect("mongodb://localhost/blogapp").then(()=>{
     })
 //Rotas
 app.get('/', (req, res) =>{
-    res.send("Rota Principal")
+    Postagem.find().lean().populate("categoria").sort({data: "desc"}).then((postagens) => {
+        res.render("index", {postagens: postagens})
+    }).catch((err) =>{
+        req.flash("error_msg", "Houve um erro interno")
+        res.redirect("/404")
+    } )
+
 })
+
+app.get("/404", (req, res) => {
+    res.send ('Erro 404')
+})
+
+
 app.get('/valmyr', (req, res)=>{
     res.send("Fala Valmyr, agora vc vai aprender node")
 })
