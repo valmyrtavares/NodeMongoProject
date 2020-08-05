56a71d029aec96e3fb0af1e8aee48447deefb8be
Valmyr Tavares <valmyr.lima@millennium.com.br>
2020-08-05 16:11:47 -0300

    Implantado o delete de postagens com get

--- routes/admin.js
+++ routes/admin.js
@@ -187,6 +187,16 @@ router.post("/postagens/edit", (req, res) =>{
 
 })
 
+router.get("/postagens/deletar/:id", (req, res)=> {
+    Postagem.remove({_id: req.params.id}).then(() => {
+        req.flash("success_msg", "Postagem deletada com sucesso")
+        res.redirect("/admin/postagens")
+    }).catch((err)=> {
+        req.flash("error_msg", "Houve um erro interno")
+        res.redirect("/admin/postagens")
+    })
+})
+
 
 
 
